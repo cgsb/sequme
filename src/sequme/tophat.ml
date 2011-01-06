f8c7ee96e3a9d9b00b0b402fd022f9739d30f6ef
Ashish Agarwal <agarwal1975@gmail.com>
2011-01-05 22:19:56 -0500

    Fixed minor bug in Tophat.

--- src/sequme/tophat.ml
+++ src/sequme/tophat.ml
@@ -17,11 +17,11 @@ type expr = {
     command line to run tophat. *)
 let cmd_string e = List.fold_left (^) "" [
   "tophat";
-  (match t.min_anchor_length with None -> "" | Some x -> sprintf " -a %d" x);
-  (if t.solexa1_3_quals then " --solexa1.3-quals" else "");
-  (match t.num_threads with None -> "" | Some x -> sprintf " -p %d" x);
-  (match t.max_multihits with None -> "" | Some x -> sprintf " -g %d" x);
-  (if t.no_coverage_search then " --no-coverage-search" else "");
-  (if t.coverage_search then " --coverage-search" else "");
-  (if t.butterfly_search then " --butterfly-search" else "")
+  (match e.min_anchor_length with None -> "" | Some x -> sprintf " -a %d" x);
+  (if e.solexa1_3_quals then " --solexa1.3-quals" else "");
+  (match e.num_threads with None -> "" | Some x -> sprintf " -p %d" x);
+  (match e.max_multihits with None -> "" | Some x -> sprintf " -g %d" x);
+  (if e.no_coverage_search then " --no-coverage-search" else "");
+  (if e.coverage_search then " --coverage-search" else "");
+  (if e.butterfly_search then " --butterfly-search" else "")
 ]
