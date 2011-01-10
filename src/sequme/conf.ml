1728d904aba85bc06f93830afb0c4caa02b20414
Ashish Agarwal <agarwal1975@gmail.com>
2011-01-09 20:35:57 -0500

    Added database configuration support in Conf. Also added sequme_root attribute to configurations.

--- src/sequme/conf.ml
+++ src/sequme/conf.ml
@@ -1,4 +1,4 @@
-open Batteries_uni
+open Batteries_uni;; open Printf
 module StringMap = Map.StringMap
 
 exception Invalid of string
@@ -10,3 +10,38 @@ let read sequme_root =
   |> File.lines_of
   |> Enum.map (flip String.split "=" |- (fun (x,y) -> String.strip x, Util.unquote (String.strip y)))
   |> Enum.fold (flip (uncurry StringMap.add)) StringMap.empty
+  |> StringMap.add "sequme_root" sequme_root
+
+type dbconf = {
+  db_engine : string;
+  db_name : string;
+  db_user : string;
+  db_password : string;
+  db_host : string;
+  db_port : string
+}
+
+let get_dbconf t =
+  let find key map =
+    try StringMap.find key map
+    with Not_found -> raise (Invalid (sprintf "attribute %s not found in given configuration" key))
+  in
+  let engine = find "database.engine" t in
+  let name = find "database.name" t in
+  match engine with
+    | "sqlite" | "sqlite3" ->
+        let name = 
+          if Filename.is_relative name then
+            Filename.concat (Filename.concat (find "sequme_root" t) "etc") name
+          else
+            name
+        in
+        {
+          db_engine="sqlite";
+          db_name=name;
+          db_user="";
+          db_password="";
+          db_host="";
+          db_port=""
+        }
+    | _ -> raise (Invalid (sprintf "%s: unsupported database engine" engine))
--- src/sequme/conf.mli
+++ src/sequme/conf.mli
@@ -4,8 +4,21 @@ open Batteries_uni
 exception Invalid of string
 
 type t = string Map.StringMap.t
-    
+
 val read : string -> t
   (** [read sequme_root] reads the configuration file located within
       the given [sequme_root] directory. Raise [Invalid] if any
       errors. *)
+
+type dbconf = {
+  db_engine : string;
+  db_name : string;
+  db_user : string;
+  db_password : string;
+  db_host : string;
+  db_port : string
+}
+
+val get_dbconf : t -> dbconf
+  (** Return dbconf in [t]. Raise [Invalid] if [t] does not fully
+      specify a database configuration. *)
