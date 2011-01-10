8cefc5cc60a0b32fa3878c8443eb0fa400440a19
Ashish Agarwal <agarwal1975@gmail.com>
2011-01-09 19:31:49 -0500

    Disallowed attribute names in conf file to be quoted.

--- src/sequme/conf.ml
+++ src/sequme/conf.ml
@@ -8,5 +8,5 @@ type t = string StringMap.t
 let read sequme_root =
   Filename.concat (Filename.concat sequme_root "etc") "sequme.conf"
   |> File.lines_of
-  |> Enum.map (flip String.split "=" |- Pair.map (String.strip |- Util.unquote))
+  |> Enum.map (flip String.split "=" |- (fun (x,y) -> String.strip x, Util.unquote (String.strip y)))
   |> Enum.fold (flip (uncurry StringMap.add)) StringMap.empty
