bffaddfd18025653b9c6f46298c84390a8f5ab05
Ashish Agarwal <agarwal1975@gmail.com>
2011-01-11 16:49:37 -0500

    Added Bowtie module. Not much in it yet.

---
+++ src/sequme/bowtie.mli
@@ -0,0 +1,7 @@
+(** Bowtie support. See the
+    {{:http://bowtie-bio.sourceforge.net}Bowtie website} for details. *)
+open Batteries_uni
+
+val path_of_index : Conf.t -> string -> string
+  (** [path_of_index i] returns path to the directory for index
+      [i]. Raise [Not_found] if index unavailable. *)
