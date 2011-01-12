6696a38b3130a1e37cafbe913858401c4edf6bf9
Ashish Agarwal <agarwal1975@gmail.com>
2011-01-12 11:26:42 -0500

    Added user_list option to Pbs. Resolves issue #1.

--- src/sequme/pbs.ml
+++ src/sequme/pbs.ml
@@ -12,6 +12,7 @@ let mail_option_to_char = function
 
 type script = {
   mail_options : mail_option list;
+  user_list : string list;
   resource_list : string option;
   job_name : string option;
   stdout_path : string option;
@@ -21,13 +22,14 @@ type script = {
   commands : string list;
 }
 
-let make_script ?(mail_options=[]) ?resource_list ?job_name
+let make_script ?(mail_options=[]) ?(user_list=[]) ?resource_list ?job_name
     ?stdout_path ?stderr_path ?(export_qsub_env=false)
     ?rerunable
     commands
     =
   {
     mail_options = List.unique mail_options;
+    user_list;
     resource_list;
     job_name = (match job_name with
       | None -> None
@@ -49,6 +51,10 @@ let script_to_string x =
       | x ->
           let x = List.map (mail_option_to_char |- string_of_char) x in
           sprintf "#PBS -m %s" (String.concat "" x)
+    );
+    (match x.user_list with
+      | [] -> ""
+      | x -> sprintf "\n#PBS -M %s" (String.concat "," x)
     );
     if x.export_qsub_env then "\n#PBS -V" else "";
     (match x.rerunable with
