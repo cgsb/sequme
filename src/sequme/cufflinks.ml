274ac62c1e302c1b7ba74328e7c88e408907917e
Ashish Agarwal <agarwal1975@gmail.com>
2011-01-14 12:17:05 -0500

    Added Cufflinks module.

---
+++ src/sequme/cufflinks.ml
@@ -0,0 +1,45 @@
+open Batteries_uni;; open Printf
+
+exception Error of string
+
+type cmd = {
+  exec : string;
+  output_dir : string option;
+  num_threads : int option;
+  mask_file : string option;
+  quartile_normalization : bool;
+  gtf : string option;
+  aligned_reads : string
+}
+
+let make_cmd
+    ?(exec="cufflinks")
+    ?output_dir
+    ?num_threads
+    ?mask_file
+    ?(quartile_normalization=false)
+    ?gtf
+    aligned_reads
+    =
+  {
+    exec;
+    output_dir;
+    num_threads;
+    mask_file;
+    quartile_normalization;
+    gtf;
+    aligned_reads
+  }
+
+let cmd_to_string cmd =
+  let s opt x = match x with None -> "" | Some x -> sprintf " -%c %s" opt x in
+  let i opt x = match x with None -> "" | Some x -> sprintf " -%c %d" opt x in
+  String.concat "" [
+    cmd.exec;
+    s 'o' cmd.output_dir;
+    i 'p' cmd.num_threads;
+    s 'M' cmd.mask_file;
+    if cmd.quartile_normalization then " -N" else "";
+    s 'G' cmd.gtf;
+    sprintf " %s" cmd.aligned_reads
+  ]
---
+++ src/sequme/cufflinks.mli
@@ -0,0 +1,24 @@
+(** Cufflinks support. See the {{:http://cufflinks.cbcb.umd.edu}Cufflinks} website for details. *)
+open Batteries_uni
+
+exception Error of string
+
+(** Cufflinks command. *)
+type cmd = private {
+  exec : string;
+  output_dir : string option;
+  num_threads : int option;
+  mask_file : string option;
+  quartile_normalization : bool;
+  gtf : string option;
+  aligned_reads : string
+}
+
+val make_cmd : ?exec:string -> ?output_dir:string -> ?num_threads:int
+  -> ?mask_file:string -> ?quartile_normalization:bool -> ?gtf:string
+  -> string -> cmd
+  (** Construct cufflinks command. See the
+      {{:http://cufflinks.cbcb.umd.edu/manual.html}Cufflinks manual for
+      details. *)
+
+val cmd_to_string : cmd -> string
