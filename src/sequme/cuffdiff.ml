67b770177ae29fbb9434570d11be9c23edcecd45
Ashish Agarwal <agarwal1975@gmail.com>
2011-01-14 12:54:08 -0500

    Added Cuffdiff module.

---
+++ src/sequme/cuffdiff.ml
@@ -0,0 +1,46 @@
+open Batteries_uni;; open Printf
+
+exception Error of string
+
+type cmd = {
+  exec : string;
+  output_dir : string option;
+  num_threads : int option;
+  mask_file : string option;
+  quartile_normalization : bool;
+  gtf : string;
+  samples : string list list
+}
+
+let make_cmd
+    ?(exec="cuffdiff")
+    ?output_dir
+    ?num_threads
+    ?mask_file
+    ?(quartile_normalization=false)
+    gtf samples
+    =
+  if List.length samples < 2 then
+    Error "must supply at least two samples to cuffdiff" |> raise
+  else {
+    exec;
+    output_dir;
+    num_threads;
+    mask_file;
+    quartile_normalization;
+    gtf;
+    samples
+  }
+
+let cmd_to_string cmd =
+  let s opt x = match x with None -> "" | Some x -> sprintf " -%c %s" opt x in
+  let i opt x = match x with None -> "" | Some x -> sprintf " -%c %d" opt x in
+  String.concat "" [
+    cmd.exec;
+    s 'o' cmd.output_dir;
+    i 'p' cmd.num_threads;
+    s 'M' cmd.mask_file;
+    if cmd.quartile_normalization then " -N" else "";
+    sprintf " %s" cmd.gtf;
+    cmd.samples |> List.map (String.concat ",") |> String.concat " " |> sprintf " %s"
+  ]
---
+++ src/sequme/cuffdiff.mli
@@ -0,0 +1,25 @@
+(** Cuffdiff support. See the
+    {{:http://cufflinks.cbcb.umd.edu}Cufflinks} website for details. *)
+open Batteries_uni
+
+exception Error of string
+
+(** Cuffdiff command. *)
+type cmd = private {
+  exec : string;
+  output_dir : string option;
+  num_threads : int option;
+  mask_file : string option;
+  quartile_normalization : bool;
+  gtf : string;
+  samples : string list list
+}
+
+val make_cmd : ?exec:string -> ?output_dir:string -> ?num_threads:int
+  -> ?mask_file:string -> ?quartile_normalization:bool
+  -> string -> string list list -> cmd
+  (** Construct cuffdiff command. See the
+      {{:http://cufflinks.cbcb.umd.edu/manual.html}Cuffdiff manual for
+      details. *)
+
+val cmd_to_string : cmd -> string
