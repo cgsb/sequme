e92ed84b5d772c58fd59faeaf06e416fc0c2e27f
Ashish Agarwal <agarwal1975@gmail.com>
2011-01-21 12:35:56 -0500

    Added priority option to Pbs.

--- src/sequme/pbs.ml
+++ src/sequme/pbs.ml
@@ -15,6 +15,7 @@ type script = {
   user_list : string list;
   resource_list : string option;
   job_name : string option;
+  priority : int option;
   stdout_path : string option;
   stderr_path : string option;
   export_qsub_env : bool;
@@ -23,6 +24,7 @@ type script = {
 }
 
 let make_script ?(mail_options=[]) ?(user_list=[]) ?resource_list ?job_name
+    ?priority
     ?stdout_path ?stderr_path ?(export_qsub_env=false)
     ?rerunable
     commands
@@ -37,6 +39,7 @@ let make_script ?(mail_options=[]) ?(user_list=[]) ?resource_list ?job_name
           if String.length x < 16 then Some x 
           else Error (sprintf "%s: job name must be <= 15 characters" x) |> raise
     );
+    priority;
     stdout_path;
     stderr_path;
     export_qsub_env;
@@ -62,6 +65,7 @@ let script_to_string x =
       | Some x -> if x then "\n#PBS -r y" else "\n#PBS -r n"
     );
     (match x.resource_list with None -> "" | Some x -> sprintf "\n#PBS -l %s" x);
+    (match x.priority with None -> "" | Some x -> sprintf "\n#PBS -p %d" x);
     (match x.stdout_path with None -> "" | Some x -> sprintf "\n#PBS -o %s" x);
     (match x.stderr_path with None -> "" | Some x -> sprintf "\n#PBS -e %s" x);
     (match x.job_name with None -> "" | Some x -> sprintf "\n#PBS -N %s" x);
