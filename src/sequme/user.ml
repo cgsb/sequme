d9491e0b8d6f0bba9696241b326fc002e39ae911
Ashish Agarwal <agarwal1975@gmail.com>
2011-03-03 15:12:52 -0500

    Added User module.

---
+++ src/sequme/user.ml
@@ -0,0 +1,16 @@
+open Batteries_uni;; open Printf
+
+exception Error of string
+
+let id_of_username dbh name =
+  match PGSQL(dbh) "SELECT id FROM auth_user where username=$name" with
+    | [] -> None
+    | x::[] -> Some x
+    | _ -> assert false
+
+(* assumes there is a group with the same name as username *)
+let group_id_of_username dbh username =
+  match PGSQL(dbh) "SELECT id FROM auth_group where name=$username" with
+    | [] -> None
+    | x::[] -> Some x
+    | _ -> assert false
