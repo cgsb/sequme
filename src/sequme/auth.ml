7d34a32b865cc5c6d420ac4df89d44f49014adca
Ashish Agarwal <agarwal1975@gmail.com>
2011-03-07 16:38:51 -0500

    Defined User.t type mimicking auth_user database objects, added of_id and of_username functions, and deleted id_of_username since it is superfluous given new functions.

--- src/sequme/auth.ml
+++ src/sequme/auth.ml
@@ -2,13 +2,39 @@ open Batteries_uni;; open Printf
 
 module User = struct
   exception Error of string
-    
-  let id_of_username dbh name =
-    match PGSQL(dbh) "SELECT id FROM auth_user where username=$name" with
+
+  type t = {
+    id : int32;
+    username : string;
+    first_name : string;
+    last_name : string;
+    email : string;
+    password : string;
+    is_staff : bool;
+    is_active : bool;
+    is_superuser : bool;
+    last_login : PGOCaml.timestamptz;
+    date_joined : PGOCaml.timestamptz;
+  }
+
+  let of_id dbh id =
+    let xs = PGSQL(dbh) "SELECT id,username,first_name,last_name,email,password,is_staff,is_active,is_superuser,last_login,date_joined
+                           FROM auth_user where id=$id"
+    in match xs with
       | [] -> None
-      | x::[] -> Some x
+      | (id,username,first_name,last_name,email,password,is_staff,is_active,is_superuser,last_login,date_joined)::[] ->
+          Some {id;username;first_name;last_name;email;password;is_staff;is_active;is_superuser;last_login;date_joined}
       | _ -> assert false
-          
+
+  let of_username dbh name =
+    let xs = PGSQL(dbh) "SELECT id,username,first_name,last_name,email,password,is_staff,is_active,is_superuser,last_login,date_joined
+                           FROM auth_user where username=$name"
+    in match xs with
+      | [] -> None
+      | (id,username,first_name,last_name,email,password,is_staff,is_active,is_superuser,last_login,date_joined)::[] ->
+          Some {id;username;first_name;last_name;email;password;is_staff;is_active;is_superuser;last_login;date_joined}
+      | _ -> assert false
+
   (* assumes there is a group with the same name as username *)
   let group_id_of_username dbh username =
     match PGSQL(dbh) "SELECT id FROM auth_group where name=$username" with
--- src/sequme/auth.mli
+++ src/sequme/auth.mli
@@ -1,12 +1,31 @@
 (** Authorization features. *)
+open Batteries_uni
 
 module User : sig
   exception Error of string
-    
-  val id_of_username : (string,bool) Hashtbl.t PGOCaml.t -> string -> int32 option
-    (** [id_of_username name] returns the user id of the user named
-        [name], or None if no such user exists. *)
-    
+
+  type t = private {
+    id : int32;
+    username : string;
+    first_name : string;
+    last_name : string;
+    email : string;
+    password : string;
+    is_staff : bool;
+    is_active : bool;
+    is_superuser : bool;
+    last_login : PGOCaml.timestamptz;
+    date_joined : PGOCaml.timestamptz;
+  }
+
+  val of_id : (string,bool) Hashtbl.t PGOCaml.t -> int32 -> t option
+    (** [of_username dbh id] returns the user with given [id], or
+        None if no such user exists. *)
+
+  val of_username : (string,bool) Hashtbl.t PGOCaml.t -> string -> t option
+    (** [of_username dbh name] returns the user with given [name], or
+        None if no such user exists. *)
+
   val group_id_of_username : (string,bool) Hashtbl.t PGOCaml.t -> string -> int32 option
     (** For every user [x], there is a group of the same name [x], whose
         sole member is [x]. [group_id_of_username dbh username] returns
