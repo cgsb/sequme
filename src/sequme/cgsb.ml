6e22969794fbfe0c1fa65526f25f714483145a70
Ashish Agarwal <agarwal1975@gmail.com>
2011-08-18 18:36:06 -0400

    Added support in Cgsb for Flowcells and bclToFastq conversion records.

--- src/sequme/cgsb.ml
+++ src/sequme/cgsb.ml
@@ -63,9 +63,108 @@ module LibraryDB = struct
     in
     let inp = open_in file in
     let _,cols,get,e = Table.of_input ~itags:"table,header,separator=\\t" inp in
-    if (cols |> List.enum |> StringSet.of_enum) <> (columns |> List.enum |> StringSet.of_enum) then
-      Error "unexpected columns" |> raise
+    let ans =
+      if (cols |> List.enum |> StringSet.of_enum) <> (columns |> List.enum |> StringSet.of_enum) then
+        Error "unexpected columns" |> raise
+      else
+        Enum.fold (add_row get) IntMap.empty e
+    in
+    close_in inp;
+    ans
+
+end
+
+module Flowcell = struct
+  exception Error of string
+
+  type record = {
+    fcid : string;
+    lane : int;
+    library : Library.t
+  }
+
+  let of_row libdb (get : Table.getter) (row : Table.row) =
+    let ans =
+      {
+        fcid = get row "FCID";
+        lane = get row "Lane" |> int_of_string;
+        library = IntMap.find (get row "Library ID" |> int_of_string) libdb;
+      }
+    in
+    let s_expected = ans.library.Library.sample.Sample.name1 in
+    let s_is = get row "Sample Name1" in
+    if s_expected <> s_is then
+      Error (sprintf "Sample Name1 %s should be %s" s_is s_expected) |> raise
     else
-      Enum.fold (add_row get) IntMap.empty e
+      ans
+
+end
+
+module FlowcellDB = struct
+  exception Error of string
+
+  type t = Flowcell.record list
+
+  let columns = ["FCID"; "Lane"; "Library ID"; "Sample Name1"]
+
+  let of_file libdb file =
+    let inp = open_in file in
+    let _,cols,get,e = Table.of_input ~itags:"table,header,separator=\\t" inp in
+    let ans =
+      if (cols |> List.enum |> StringSet.of_enum) <> (columns |> List.enum |> StringSet.of_enum) then
+        Error "unexpected columns" |> raise
+      else
+        Enum.fold (fun ans row -> (Flowcell.of_row libdb get row)::ans) [] e
+    in
+    close_in inp;
+    List.rev ans
+
+end
+
+module BclToFastq = struct
+  exception Error of string
+
+  type t = {
+    fcid : string;
+    mismatch : int;
+    sample_sheet_path : string;
+  }
+
+  let of_row get dir row =
+    {
+      fcid = get row "FCID";
+      mismatch = get row "mismatch" |> int_of_string;
+      sample_sheet_path = Filename.concat dir "SampleSheet.csv"
+    }
+
+end
+
+module BclToFastqDB = struct
+  exception Error of string
+
+  type t = BclToFastq.t IntMap.t
+
+  let columns = ["ID"; "FCID"; "mismatch"]
+
+  (* TO DO: change this, hard-coded path is BAD! *)
+  let dir = "/data/hiseq"
+
+  let of_file file =
+    let add_row get ans row =
+      let id = get row "ID" |> int_of_string in
+      let dir = Filename.concat dir (sprintf "%d" id) in
+      let bq = BclToFastq.of_row get dir row in
+      IntMap.add id bq ans
+    in
+    let inp = open_in file in
+    let _,cols,get,e = Table.of_input ~itags:"table,header,separator=\\t" inp in
+    let ans =
+      if (cols |> List.enum |> StringSet.of_enum) <> (columns |> List.enum |> StringSet.of_enum) then
+        Error "unexpected columns" |> raise
+      else
+        Enum.fold (add_row get) IntMap.empty e
+    in
+    close_in inp;
+    ans
 
 end
--- src/sequme/cgsb.mli
+++ src/sequme/cgsb.mli
@@ -29,3 +29,35 @@ module LibraryDB : sig
   val of_file : string -> t
 
 end
+
+module Flowcell : sig
+  exception Error of string
+
+  type record = {
+    fcid : string;
+    lane : int;
+    library : Library.t
+  }
+end
+
+module FlowcellDB : sig
+  exception Error of string
+  type t = Flowcell.record list
+  val of_file : LibraryDB.t -> string -> t
+end
+
+module BclToFastq : sig
+  exception Error of string
+
+  type t = {
+    fcid : string;
+    mismatch : int;
+    sample_sheet_path : string;
+  }
+end
+
+module BclToFastqDB : sig
+  exception Error of string
+  type t = BclToFastq.t Map.IntMap.t
+  val of_file : string -> t
+end
