e518d9166a4b225028228f43f780852220b2df53
Ashish Agarwal <agarwal1975@gmail.com>
2011-10-13 14:52:08 -0400

    Improved paths in Th17's cufflinks script.

--- src/app/th17/cufflinks.ml
+++ src/app/th17/cufflinks.ml
@@ -2,49 +2,52 @@
 Ocaml.packs := ["batteries"; "sequme"]
 --
 open Batteries_uni;; open Printf;; open Sequme
-open Filename
 
-let root = List.reduce concat ["/data"; "users"; "aa144"; "th17"]
+let mkpath = List.fold_left Filename.concat
 
-let run genome sl_id =
+let run root meta sl_id =
+  let m = List.filter (fun x -> x.Th17_meta.sl_id = sl_id) meta in
+  assert (List.length m = 1);
+  let m = List.hd m in
+
   let exec = "/share/apps/cufflinks/0.9.3/Linux_x86_64_precompiled/cufflinks" in
   let num_threads = 5 in
   let quartile_normalization = false in
-  let gtf = match genome with
-    | "mm9" -> "/data/sequme/db/th17/cachedfile/refGeneAnnot.gtf"
-    | "hg19" -> "/data/users/aa144/th17/refseq/hg19_refseq_annot_May_1_2011.gtf"
-    | _ -> failwith (sprintf "unknown genome %s" genome)
+  let gtf = match m.Th17_meta.organism with
+    | "Mouse" -> mkpath root ["cachedfile"; "refGeneAnnot.gtf"]
+    | "Human" -> mkpath root ["refseq"; "hg19_refseq_annot_May_1_2011.gtf"]
+    | _ -> assert false
   in
 
-  let outdir = List.reduce concat [root; "cufflinks"; sl_id] in
+  let outdir = mkpath root ["cufflinks"; sl_id] in
   Unix.mkdir outdir 0o755;
 
-  let cufflinks_outdir = concat outdir "cufflinks_out" in
+  let cufflinks_outdir = mkpath outdir ["cufflinks_out"] in
 
-  let cmd = match genome with
-    | "mm9" -> Cufflinks.make_cmd ~exec
+  let cmd = match m.Th17_meta.organism with
+    | "Mouse" -> Cufflinks.make_cmd ~exec
         ~output_dir:cufflinks_outdir
           ~num_threads
-          ~mask_file:"/data/sequme/db/th17/cachedfile/20101217_rRNA_tRNA_mask.gtf"
+          ~mask_file:(mkpath root ["cachedfile"; "20101217_rRNA_tRNA_mask.gtf"])
           ~quartile_normalization
           ~gtf
-          (List.reduce concat [root; "tophat"; sl_id; "tophat_out"; "accepted_hits.bam"])
-    | "hg19" ->
+          (mkpath root ["tophat"; sl_id; "tophat_out"; "accepted_hits.bam"])
+    | "Human" ->
         Cufflinks.make_cmd ~exec
           ~output_dir:cufflinks_outdir
           ~num_threads
           ~quartile_normalization
           ~gtf
-          (List.reduce concat [root; "tophat"; sl_id; "tophat_out"; "accepted_hits.bam"])
-    | _ -> failwith (sprintf "unknown genome %s" genome)
+          (mkpath root ["tophat"; sl_id; "tophat_out"; "accepted_hits.bam"])
+    | _ -> assert false
   in
 
   let job_name = sprintf "cufflinks_%s" sl_id |> flip String.right 15 in
   let resource_list = "nodes=1:ppn=6" in
-  let pbs_outdir = concat outdir "pbs_out" in
+  let pbs_outdir = mkpath outdir ["pbs_out"] in
   let rename file = sprintf "mv %s %s"
-    (concat cufflinks_outdir file)
-    (concat cufflinks_outdir (sl_id ^ "_" ^ file))
+    (mkpath cufflinks_outdir [file])
+    (mkpath cufflinks_outdir [sl_id ^ "_" ^ file])
   in
   let cmds = [
     Cufflinks.cmd_to_string cmd;
@@ -62,4 +65,7 @@ let run genome sl_id =
 
 ;;
 
-run Sys.argv.(1) Sys.argv.(2)
+let root_dir = Sys.argv.(1)
+let meta = mkpath root_dir ["metadata"; "metadata.tsv"] |> Th17_meta.of_file
+let sl_id = Sys.argv.(2)
+let _ = run root_dir meta sl_id
