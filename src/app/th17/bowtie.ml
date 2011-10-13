d08aa1bdd2086ca65423d9904a9f0583e82ab954
Ashish Agarwal <agarwal1975@gmail.com>
2011-10-13 12:01:47 -0400

    Improved path references to Th17's Bowtie script.

--- src/app/th17/bowtie.ml
+++ src/app/th17/bowtie.ml
@@ -3,6 +3,8 @@ Ocaml.packs := ["batteries"; "biocaml"; "sequme"]
 --
 open Batteries_uni;; open Printf;; open Sequme
 
+let mkpath = List.fold_left Filename.concat
+
 let run root meta sl_id =
   let m = List.filter (fun x -> x.Th17_meta.sl_id = sl_id) meta in
   assert (List.length m = 1);
@@ -19,16 +21,16 @@ let run root meta sl_id =
     | _ -> assert false
   in
 
-  let outdir = List.reduce Filename.concat [root; "bowtie"; sl_id] in
+  let outdir = mkpath root ["bowtie"; sl_id] in
   let _ = Unix.mkdir outdir 0o755 in
   
   let ebwt = match m.Th17_meta.organism with
-    | "Mouse" -> "/data/sequme/db/bowtie/indexes/mm9/mm9"
-    | "Human" -> "/data/sequme/db/bowtie/indexes/hg19/hg19"
+    | "Mouse" -> mkpath root ["bowtie_index"; "mm9"; "mm9"]
+    | "Human" -> mkpath root ["bowtie_index"; "hg19"; "hg19"]
     | _ -> assert false
   in
 
-  let hit_file = Filename.concat outdir (sl_id ^ ".sam") in
+  let hit_file = mkpath outdir [sl_id ^ ".sam"] in
 
   let cmd = Bowtie.make_cmd ~exec
     ~ebwt
@@ -37,12 +39,12 @@ let run root meta sl_id =
     ~best ~sam
     ~threads:num_threads
     ~hit:hit_file
-    ~reads:(List.reduce Filename.concat [root; "fastq"; sl_id ^ ".fastq"])
+    ~reads:(mkpath root ["fastq"; sl_id ^ ".fastq"])
   in
 
   let job_name = sprintf "bowtie_%s" sl_id |> flip String.right 15 in
   let resource_list = "nodes=1:ppn=8,mem=14gb" in
-  let pbs_outdir = Filename.concat outdir "pbs_out" in
+  let pbs_outdir = mkpath outdir ["pbs_out"] in
   let cmds = [
     Bowtie.cmd_to_string cmd;
     "";
@@ -55,6 +57,6 @@ let run root meta sl_id =
 ;;
 
 let root_dir = Sys.argv.(1)
-let meta = List.reduce Filename.concat [root_dir; "metadata"; "metadata.tsv"] |> Th17_meta.of_file
+let meta = mkpath root_dir ["metadata"; "metadata.tsv"] |> Th17_meta.of_file
 let sl_id = Sys.argv.(2)
-(* let _ = run root_dir meta sl_id *)
+let _ = run root_dir meta sl_id
