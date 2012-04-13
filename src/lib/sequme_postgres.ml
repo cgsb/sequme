50ad736c359526ae64a62e590b333dbf36e3f0a2
Ashish Agarwal <agarwal1975@gmail.com>
2012-04-13 17:19:18 -0400

    [Postgres] Implemented abstract syntax tree for a small subset of the PostgreSQL language.

--- src/lib/sequme_postgres.ml
+++ src/lib/sequme_postgres.ml
@@ -11,3 +11,157 @@ let exists_db db =
   else if stdout = "0" then false
   else if stderr = err_str then false
   else failwith (sprintf "Sequme_postgres.exists_db returned\n  stdout: %s\n  stderr: %s\n" stdout stderr)
+
+module Val = struct
+  type t = Int of int | Now
+
+  let to_string = function
+    | Int x -> string_of_int x
+    | Now -> "now()"
+
+end
+
+module Column = struct
+
+  type base_type = 
+      | SmallInt | Integer | BigInt
+      | Float4 | Float8 | Numeric of int * int
+      | Char of int | VarChar of int | Text
+      | Serial | BigSerial
+      | Boolean | Bytea | Money
+      | Bit of int | VarBit of int
+      | Date | Time | TimeStamp | Interval
+      | Cidr | Inet | MacAddr
+      | Box | Circle | Line | LSeg | Path | Point | Polygon
+      | TSQuery | TSVector | TxID_Snapshot
+      | Uuid | Xml
+
+  type typ =
+      | Scalar of base_type
+      | Array of base_type
+
+  type modifier =
+      | NotNull
+      | Unique
+      | PrimaryKey
+      | Default of Val.t
+      | WithTimeZone
+      | References of string * string (** foreign key reference to (table,column) *)
+
+  type decl = string * typ * modifier list
+
+  let smallint = Scalar SmallInt
+  let integer = Scalar Integer
+  let bigint = Scalar BigInt
+  let float4 = Scalar Float4
+  let real = float4
+  let float8 = Scalar Float8
+  let double_precision = Scalar Float8
+  let numeric m n = Scalar (Numeric (m,n))
+  let char n = Scalar (Char n)
+  let varchar n = Scalar (VarChar n)
+  let text = Scalar Text
+  let serial = Scalar Serial
+  let bigserial = Scalar BigSerial
+  let boolean = Scalar Boolean
+  let bytea = Scalar Bytea
+  let money = Scalar Money
+  let bit n = Scalar (Bit n)
+  let varbit n = Scalar (VarBit n)
+  let date = Scalar Date
+  let time = Scalar Time
+  let timestamp = Scalar TimeStamp
+  let interval = Scalar Interval
+  let cidr = Scalar Cidr
+  let inet = Scalar Inet
+  let macaddr = Scalar MacAddr
+  let box = Scalar Box
+  let cirlce = Scalar Circle
+  let line = Scalar Line
+  let point = Scalar Point
+  let lseg = Scalar LSeg
+  let path = Scalar Path
+  let point = Scalar Point
+  let polygon = Scalar Polygon
+  let tsquery = Scalar TSQuery
+  let tsvector = Scalar TSVector
+  let txid_snapshot = Scalar TxID_Snapshot
+  let uuid = Scalar Uuid
+  let xml = Scalar Xml
+
+  let base_type_to_string = function
+    | SmallInt -> "smallint"
+    | Integer -> "integer"
+    | BigInt -> "bigint"
+    | Float4 -> "real"
+    | Float8 -> "double precision"
+    | Numeric (m,n) -> sprintf "numeric (%d,%d)" m n
+    | Char n -> sprintf "character (%d)" n
+    | VarChar n -> sprintf "character varying (%d)" n
+    | Text -> "text"
+    | Serial -> "serial"
+    | BigSerial -> "bigserial"
+    | Boolean -> "boolean"
+    | Bytea -> "bytea"
+    | Money -> "money"
+    | Bit n -> sprintf "bit (%d)" n
+    | VarBit n -> sprintf "bit varying (%d)" n
+    | Date -> "date"
+    | Time -> "time"
+    | TimeStamp -> "timestamp"
+    | Interval -> "interval"
+    | Cidr -> "cidr"
+    | Inet -> "inet"
+    | MacAddr -> "macaddr"
+    | Box -> "box"
+    | Circle -> "circle"
+    | Line -> "line"
+    | LSeg -> "lseg"
+    | Path -> "path"
+    | Point -> "point"
+    | Polygon -> "polygon"
+    | TSQuery -> "tsquery"
+    | TSVector -> "tsvector"
+    | TxID_Snapshot -> "txid_snapshot"
+    | Uuid -> "uuid"
+    | Xml -> "xml"
+
+  let typ_to_string = function
+    | Scalar x -> base_type_to_string x
+    | Array x -> sprintf "%s[]" (base_type_to_string x)
+
+  let modifier_to_string = function
+    | NotNull -> "not null"
+    | Unique -> "unique"
+    | PrimaryKey -> "primary key"
+    | Default x -> sprintf "default %s" (Val.to_string x) 
+    | WithTimeZone -> "with time zone"
+    | References (table,column) -> sprintf "references %s(%s)" table column
+
+  let decl_to_string (name,typ,modifiers) =
+    sprintf "\"%s\" %s%s%s"
+      name
+      (typ_to_string typ)
+      (if List.length modifiers > 0 then " " else "")
+      (modifiers |> List.map ~f:modifier_to_string |> String.concat ~sep:" ")
+
+end
+
+module Table = struct
+
+  type decl = {
+    name : string;
+    temporary : bool;
+    columns : Column.decl list;
+  }
+
+  let make ~name ?(temporary=false) ~columns () =
+    {name; temporary; columns}
+
+  let create_table_stmt x =
+    sprintf "CREATE %sTABLE %s (%s)"
+      (if x.temporary then "TEMP " else "")
+      x.name
+      (x.columns |> List.map ~f:Column.decl_to_string |> String.concat ~sep:", ")
+
+end
--- src/lib/sequme_postgres.mli
+++ src/lib/sequme_postgres.mli
@@ -1,4 +1,7 @@
-(** PostgreSQL support. *)
+(** PostgreSQL support. The abstract syntax tree defined here meets
+    only our specific needs. It is not a comprehensive representation
+    of the PostgreSQL language, and the API allows creation of invalid
+    SQL statements. *)
 
 val exists_db : string -> bool
   (** [exists_db db] returns true if database [db] exists. Return
@@ -9,3 +12,136 @@ val exists_db : string -> bool
       possible as the implementation is highly fragile. Do not use
       this in production code.
   *)
+
+(** Values *)
+module Val : sig
+
+  type t =
+      | Int of int
+      | Now
+
+end
+
+(** Column types *)
+module Column : sig
+
+  type base_type = 
+      | SmallInt (** signed 2-byte integer *)
+      | Integer (** signed 4-byte integer *)
+      | BigInt (** signed 8-byte integer *)
+      | Float4 (** single precision 4-byte float, aka real *)
+      | Float8 (** double precision 8-byte float *)
+      | Numeric of int * int
+      | Char of int (** fixed-length string of length n *)
+      | VarChar of int (** variable length string up to n bits *)
+      | Text (** variable length string with no maximum length *)
+      | Serial (** auto-incrementing 4-byte integer *)
+      | BigSerial (** auto-incrementing 8-byte integer *)
+      | Boolean
+      | Bytea (** byte array *)
+      | Money
+      | Bit of int (** fixed-length bit string of length n *)
+      | VarBit of int (** variable length bit string up to n bits *)
+      | Date (** year, month, day *)
+      | Time (** time of day *)
+      | TimeStamp (** date and time *)
+      | Interval (** time span *)
+      | Cidr (** IPv4 or IPv6 network address *)
+      | Inet (** IPv4 or IPv6 host address *)
+      | MacAddr
+      | Box
+      | Circle
+      | Line
+      | LSeg
+      | Path
+      | Point
+      | Polygon
+      | TSQuery
+      | TSVector
+      | TxID_Snapshot
+      | Uuid
+      | Xml
+
+
+  type typ =
+      | Scalar of base_type
+      | Array of base_type
+
+  (** Column constraints. Not called "constraint" because that is an
+      OCaml keyword. *)
+  type modifier =
+      | NotNull
+      | Unique
+      | PrimaryKey
+      | Default of Val.t
+      | WithTimeZone
+      | References of string * string (** foreign key reference to (table,column) *)
+
+  (** A column declaration consists of a column name, column type, and
+      a list of constraints. *)
+  type decl = string * typ * modifier list
+
+  val smallint : typ
+  val integer : typ
+  val bigint : typ
+  val float4 : typ
+  val real : typ
+  val float8 : typ
+  val double_precision : typ
+  val numeric : int -> int -> typ
+  val char : int -> typ
+  val varchar : int -> typ
+  val text : typ
+  val serial : typ
+  val bigserial : typ
+  val boolean : typ
+  val bytea : typ
+  val money : typ
+  val bit : int -> typ
+  val varbit : int -> typ
+  val date : typ
+  val time : typ
+  val timestamp : typ
+  val interval : typ
+  val cidr : typ
+  val inet : typ
+  val macaddr : typ
+  val box : typ
+  val cirlce : typ
+  val line : typ
+  val point : typ
+  val lseg : typ
+  val path : typ
+  val point : typ
+  val polygon : typ
+  val tsquery : typ
+  val tsvector : typ
+  val txid_snapshot : typ
+  val uuid : typ
+  val xml : typ
+
+end
+
+(** Tables *)
+module Table : sig
+
+  (** A table declaration. *)
+  type decl = private {
+    name : string; (** name of the table *)
+    temporary : bool; (** true if the table is temporary *)
+    columns : Column.decl list; (** columns of the table *)
+  }
+
+  (** [make name columns] returns a table type. Default value for
+      [temporary] is false.
+
+      @raise Failure if given parameters do not represent a valid
+      table. (WARNING: currently no checks are being done.)
+  *)
+  val make : name:string -> ?temporary:bool -> columns:(Column.decl list) -> unit -> decl
+
+  (** Given a table declaration, return the PostgreSQL statement to
+      create it. *)
+  val create_table_stmt : decl -> string
+
+end
