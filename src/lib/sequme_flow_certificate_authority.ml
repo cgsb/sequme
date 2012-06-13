8263cc1d102e2aca86677a927c2045e4648ba778
Sebastien Mondet <seb@mondet.org>
2012-06-13 18:16:06 -0400

    Create the Flow_certificate_authority module
    The module is mostly empty.

---
+++ src/lib/sequme_flow_certificate_authority.ml
@@ -0,0 +1,18 @@
+
+type t = {
+
+  path: string;
+
+}
+
+let create path =
+  (* create necessary files *)
+  { path }
+
+let load path =
+  (* should read file(s) from there *)
+  { path }
+
+
+
+    
