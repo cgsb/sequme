7940ba18ae2bbc33fd300fad3d11407c8ce0f6c3
Sebastien Mondet <seb@mondet.org>
2012-06-26 15:55:51 -0400

    Add functions Sequme-pbs.{parse_qstat,get_status}

--- src/lib/sequme_pbs.ml
+++ src/lib/sequme_pbs.ml
@@ -114,3 +114,53 @@ let make_and_run ?(resource_list="nodes=1:ppn=8,mem=14gb") ~job_name ?(export_qs
   match Sys.command cmd with
     | 0 -> ()
     | x -> eprintf "qsub returned exit code %d\n" x
+
+
+type job_stats_raw = string * (string * string) list
+(** The output of [parse_pbs]: ["Job official ID", (key, value) list]. *)
+
+(** Parse the output of [qstat -f1 <job-id>] (for now this does not
+    handle output for multiple jobs). *)
+let parse_qstat (s: string) : (job_stats_raw, _) Core.Std.Result.t =
+  let open Core.Std in
+  let open Result in
+  let option_or_fail e o =
+    match o with Some s -> return s | None -> fail e in
+  let lines =
+    String.split ~on:'\n' s |! List.map ~f:String.strip
+    |! List.filter ~f:((<>) "") in
+  List.hd lines |! option_or_fail (`no_header s)
+  >>= fun header ->
+  String.lsplit2 header ~on:':' |! option_or_fail (`wrong_header_format header)
+  >>| snd
+  >>| String.strip
+  >>= fun official_job_id ->
+  List.tl lines |! option_or_fail (`no_header s)
+  >>= fun actual_info ->
+  let oks, errors =
+    List.map actual_info (fun line ->
+      String.lsplit2 line ~on:'=' |! option_or_fail (`wrong_line_format line)
+      >>= fun (key, value) ->
+      return (String.strip key, String.strip value))
+    |! List.partition_map  ~f:(function | Ok o -> `Fst o | Error e -> `Snd e) in
+  begin if errors <> []
+    then fail (`errors errors)
+    else return (official_job_id, oks)
+  end
+
+(** Get the status of the job (this follows
+{{:http://linux.die.net/man/1/qstat-torque}the qstat-torque manpage}).*)
+let get_status ((_, assoc): job_stats_raw) =
+  let open Core.Std.Result in
+  match Core.Std.List.Assoc.find assoc "job_state" with
+  | Some "R" -> return `Running
+  | Some "Q" -> return `Queued
+  | Some "C" -> return `Completed
+  | Some "E" -> return `Exiting
+  | Some "H" -> return `Held
+  | Some "T" -> return `Moved
+  | Some "W" -> return `Waiting
+  | Some "S" -> return `Suspended
+  | Some s -> fail (`unknown_status s)
+  | None -> fail `job_state_not_found
+
