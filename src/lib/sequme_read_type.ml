263c35185e8586ec21dcf396d222e16fb8a7b41a
Sebastien Mondet <seb@mondet.org>
2013-03-18 14:10:16 -0400

    Read_type: port from Std to Internal_pervasives

--- src/lib/sequme_read_type.ml
+++ src/lib/sequme_read_type.ml
@@ -1,14 +1,14 @@
-open Sequme_std
+open Sequme_internal_pervasives
 
 exception Error of string
 
 type t = SingleEnd | PairedEnd
 
 let of_string x =
-  let x' = String.map Char.lowercase x in
-  if x' = "se" || String.starts_with x "single" then
+  let x' = String.map ~f:Char.lowercase x in
+  if x' = "se" || String.is_prefix x ~prefix:"single" then
     SingleEnd
-  else if x' = "pe" || String.starts_with x "paired" then
+  else if x' = "pe" || String.is_prefix x ~prefix:"paired" then
     PairedEnd
   else
     Error (sprintf "unknown read type %s" x) |> raise
